ce2ae96cdba23a22f6bc8e84fa6dcf3900a56489
vitcum <vitcum@gmail.com>
2016-11-24 21:43:29 +0100

    Delete scripts4.js

--- js/scripts4.js
+++
@@ -1,9 +0,0 @@
-function getTriangleArea(a, h) {
-	if ( a <= 0 || h <= 0) {
-		console.log("Nieprawidłowe dane")
-	} else if ( a > 0 && h > 0) {
-		return a*h/2
-	}
-}
-
-console.log( getTriangleArea(10,6) )
